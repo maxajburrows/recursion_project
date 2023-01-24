d78a6202fb9239855ae72557b5034339a81fe1c2
Max Burrows <maxajburrows@gmail.com>
2023-01-24 15:22:45 +0100

    Simplify recursive calling of left and right arrays

--- merge_sort.js
+++ merge_sort.js
@@ -3,16 +3,10 @@
 //Merge sort algorithm
 function mergeSort(array) {
     if (array.length <= 1) return array; //Base case: return if array is just a singular element or is empty.
-    let arrayLeft;
-    let arrayRight;
     //Sort left and right halves of array separately.
-    if (array.length%2 === 0) {
-        arrayLeft = mergeSort(array.slice(0, array.length/2));
-        arrayRight = mergeSort(array.slice(array.length/2, array.length));
-    } else {
-        arrayLeft = mergeSort(array.slice(0, Math.round(array.length/2)));
-        arrayRight = mergeSort(array.slice(Math.round(array.length/2), array.length));
-    } 
+    let arrayLeft = mergeSort(array.slice(0, Math.floor(array.length/2)));
+    let arrayRight = mergeSort(array.slice(Math.floor(array.length/2), array.length));  
+    //Merge the left and right lists
     let arrayMerged = [];
     while (arrayLeft.length > 0 && arrayRight.length > 0) {
         if (arrayLeft[0] <= arrayRight[0]) {
@@ -33,3 +27,4 @@ function mergeSort(array) {
 let array1 = [8, 7, 10, 4, 3, 2, 17, 6];
 let array2 = [87, 94, 12, 8, 7, 10, 4, 3, 2, 17, 6];
 let array3 = [4];
+let array4 = [];
