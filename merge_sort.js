4097b4f5f0e2df142e7c9c3409bc81df0df4e3ff
Max Burrows <maxajburrows@gmail.com>
2023-01-25 09:47:50 +0100

    Simplify if statements

--- merge_sort.js
+++ merge_sort.js
@@ -1,5 +1,4 @@
 
-
 //Merge sort algorithm
 function mergeSort(array) {
     if (array.length <= 1) return array; //Base case: return if array is just a singular element or is empty.
@@ -14,20 +13,13 @@ function mergeSort(array) {
 function merge(arrayLeft, arrayRight) {
     let arrayMerged = [];
     while (arrayLeft.length > 0 && arrayRight.length > 0) {
-        if (arrayLeft[0] <= arrayRight[0]) {
-            arrayMerged.push(arrayLeft.shift());
-        } else {
-            arrayMerged.push(arrayRight.shift());
-        }
+        (arrayLeft[0] <= arrayRight[0]) ? arrayMerged.push(arrayLeft.shift()):
+                                          arrayMerged.push(arrayRight.shift());
     }
-    if (arrayLeft.length > 0) {
-        arrayMerged = [...arrayMerged, ...arrayLeft];
-    } else { 
-        arrayMerged = [...arrayMerged, ...arrayRight];
-    }
+    arrayMerged = (arrayLeft.length > 0) ? [...arrayMerged, ...arrayLeft]:   
+                                           [...arrayMerged, ...arrayRight];
     return arrayMerged
 }
-
 
 //Arrays for testing the merge sort function
 let array1 = [8, 7, 10, 4, 3, 2, 17, 6];
